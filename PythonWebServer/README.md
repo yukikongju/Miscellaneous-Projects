9bbe1851f334b6c714825669fb9cba750dbad053
Emulie Chhor <emulie.chhor@hotmail.com>
2022-06-24 14:48:39 -0400

    initialized web server

---
+++ PythonWebServer/README.md
@@ -0,0 +1,6 @@
+# Python Client Web Server
+
+
+
+
+- [Neural Nine - Simple HTTP Server in Python](https://www.youtube.com/watch?v=DeFST8tvtuI&t=622s)
---
+++ PythonWebServer/webserver.py
@@ -0,0 +1,19 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class CustomServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write(bytes("<html><body><h1>test</h1></body></html>", "utf-8"))
+        
+
+HOST, PORT = 'localhost', 8080
+httpd = HTTPServer((HOST, PORT), CustomServer)
+print(f"Running Server. Go to url from browser: {HOST}:{PORT}")
+print(f"using curl: curl {HOST}:{PORT}")
+httpd.serve_forever()
+
