5fc41c2701265e95616bf5d6039a82903f360162
Emulie Chhor <emulie.chhor@hotmail.com>
2022-12-19 11:15:00 -0500

    added code to move dir into more_courses dir

--- bash-scripting/mit_rename.sh
+++ bash-scripting/mit_rename.sh
@@ -30,24 +30,42 @@ exams_dir="Exams"
 solutions_dir="Solutions"
 recitations_dir="Recitations"
 misc_dir="Miscellaneous"
-mkdir ${slides_dir} ${problems_dir} ${reading_dir} ${labs_dir} ${exams_dir} ${solutions} ${recitations_dir} ${misc_dir}
+homework_dir="Homework"
+mkdir ${slides_dir} ${problems_dir} ${reading_dir} ${labs_dir} ${exams_dir} ${solutions} ${recitations_dir} ${misc_dir} ${homework_dir}
 
 echo "moving file into their respective directory"
 for file in $(ls *.pdf)
 do 
     suffix=${file#*_}
-    if [[ "${suffix}" == *"text"* ]]; then
-	mv ${file} "${reading_dir}/${file}"
-    elif [[ "${suffix}" == *"appl"* ]]; then
-	mv ${file} "${labs_dir}/${file}"
-    elif [[ "${suffix}" == *"rec"* ]]; then
+    if [[ "${suffix}" == *"sol"* ]]; then
 	mv ${file} "${recitations_dir}/${file}"
     elif [[ "${suffix}" == *"quiz"* ]]; then
 	mv ${file} "${exams_dir}/${file}"
-    elif [[ "${suffix}" == *"ps"* ]]; then 
+    elif [[ "${suffix}" == *"Problem"* ]]; then 
 	mv ${file} "${problems_dir}/${file}"
+    elif [[ "${suffix}" == *"hw"* ]]; then 
+	mv ${file} "${homework_dir}/${file}"
+    elif [[ "${suffix}" == *"Lecture"* ]]; then
+	mv ${file} "${reading_dir}/${file}"
     else
 	mv ${file} "${misc_dir}/${file}"
     fi
 done
+
+# remove useless directory
+rmdir *
+
+# move into files
+course_dir="MIT 18.311 - Principles of Applied Maths"
+mkdir "${course_dir}"
+
+for dir in $(find . -type d)
+do
+    echo $dir
+    mv "$dir" "${course_dir}"
+done
+
+# move into "More Courses"
+more_course_dir="/home/yukikongju/Documents/OneDrive/Bac-Maths-Info/More Courses"
+mv "${course_dir}" "${more_course_dir}"
 
