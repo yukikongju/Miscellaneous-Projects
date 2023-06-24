0f7c3b1fcb596ee998bf853b0e0aa8c231d68b83
Emulie Chhor <emulie.chhor@hotmail.com>
2023-06-24 01:36:01 -0400

    added sleepadmin function

--- SleepAdminDeltaCalculations/utils.py
+++ SleepAdminDeltaCalculations/utils.py
@@ -1,78 +1,78 @@
-import statsmodels.stats.proportion as smp
+import numpy as np
+#  import statsmodels.stats.proportion as smp
 
+import statsmodels.stats.proportion as prop
 from statsmodels.stats.power import tt_ind_solve_power, zt_ind_solve_power
 
-def compute_deltas(sample_size: int, conversion_rate: float, power=0.8, alpha=0.5, alternative='smaller'):
+
+def calculateMetricParameters(baseline: float, delta: float, num_observations: int, power=0.8, alpha=0.05):
     """
-    Compute absolute and relative deltas given sample size and conversion_rate 
-    (baseline)
-
-    doc: https://www.statsmodels.org/stable/generated/statsmodels.stats.power.tt_ind_solve_power.html
-
+    This Function is used in Sleep Admin to compute the following:
+    Given baseline and power, compute:
+    - Number of observations if delta is given
+    - delta if number of observations is given
     """
-    # --- TODO: verify if and effect_size are okay
 
-    # --- absolute delta calculation: using 'larger' instead of 'two-sided'
-    absolute_delta = zt_ind_solve_power(effect_size=None, nobs1=sample_size,
-                                        alpha=alpha, power=power, ratio=1.0, 
-                                        alternative=alternative)
+    def get_prop_effect_size(baseline: float, delta: float) -> float:
+        variant = baseline + delta
 
-    absolute_delta *= conversion_rate
+        std_effect = prop.proportion_effectsize(prop1=baseline, prop2=variant)
+        std_effect = float(std_effect)
 
-    # --- relative delta calculation
-    relative_delta = float(absolute_delta / conversion_rate)
+        return std_effect
 
-    return absolute_delta, relative_delta
+    def get_prop_effect_size_invert(baseline: float, effect_size: float) -> float:
+        delta = np.sin(np.arcsin(np.sqrt(baseline)) -
+                       effect_size / 2) ** 2 - baseline
+        delta = float(np.abs(delta))
 
+        return delta
 
-def compute_weekly_reach_per_variant(monthly_reach: int):
-    """
-    Calculate the weekly reach that goes in each variant bucket
+    def ind_solve_power(baseline: float, delta: float, nobs1: int, alpha: float, power: float, ratio: float,
+                        alternative: str) -> float:
+        if delta is not None:
+            effect_size = get_prop_effect_size(baseline=baseline, delta=delta)
+        else:
+            effect_size = None
 
-    Note: we compute the initial weekly reach with only the control and 
-    the variant, even if there is more than 2 variants (ex: control, variant a, 
-    variant b)
+        new_param = zt_ind_solve_power(effect_size=effect_size, nobs1=nobs1, alpha=alpha, power=power,
+                                       ratio=ratio, alternative=alternative)
 
-    Weekly Reach = (Monthly Reach / 2 variants) / 4 weeks
-    """
-    return (monthly_reach / 2) / 4
-    
+        if delta is None:
+            new_param = get_prop_effect_size_invert(
+                baseline=baseline, effect_size=new_param)
 
-def compute_sample_needed(conversion_rate: float, absolute_delta: float, power=0.8, alpha=0.5):
-    """ 
-    Compute sample needed to reach a given absolute delta
-    """
-    sample_size =  smp.samplesize_proportions_2indep_onetail(diff=absolute_delta, prop2=conversion_rate, power=power)
+        return new_param
 
-    return sample_size
+    def validate_inputs(baseline: float, delta: float, nobs1: float, alpha: float, power: float) -> None:
+        def if_exist_and_not_ratio_raise_error(param_name: str, value: float) -> bool:
 
-#  ---------------------------------------------------------------------------
+            if value:
+                if not 0 < alpha < 1:
+                    raise ValueError(f"{param_name} is not between 0 and 1")
 
+        if_exist_and_not_ratio_raise_error("delta", delta)
+        if_exist_and_not_ratio_raise_error("alpha", alpha)
+        if_exist_and_not_ratio_raise_error("power", power)
 
-def compute_absolute_delta_from_relative_delta(relative_delta: float, conversion_rate: float):
-    """ 
-    Compute absolute delta from relative delta
-    """
-    absolute_delta = relative_delta * conversion_rate
-    return absolute_delta
+        if baseline is None:
+            raise ValueError("Baseline should be defined")
 
+        if delta:
+            if 1 <= baseline + delta:
+                raise ValueError("Delta + baseline is greater or equal than 1")
 
-#  ---------------------------------------------------------------------------
+        if nobs1:
+            if nobs1 < 0:
+                raise ValueError("num_observations is not greater than 1")
 
+    ALTERNATIVE = 'smaller'
+    RATIO = 1.0
 
-def test_compute_deltas():
-    print(compute_deltas(6883, 0.0486))
+    validate_inputs(baseline, delta, num_observations, alpha, power)
 
+    new_param = ind_solve_power(baseline=baseline, delta=delta, nobs1=num_observations, alpha=alpha, power=power,
+                                ratio=RATIO, alternative=ALTERNATIVE)
 
-
-def tests():
-    test_compute_deltas()
-    
-
-#  ---------------------------------------------------------------------------
-
-
-if __name__ == "__main__":
-    tests()
-
+    return new_param
 
