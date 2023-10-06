9bc8ac8d5161776dd6e46723c4cc3bdfefdea1ed
Emulie Chhor <emulie.chhor@hotmail.com>
2023-10-06 18:05:42 -0400

    added guideline for experiments simulation

--- MonteCarloCoins/README.md
+++ MonteCarloCoins/README.md
@@ -1,8 +1,31 @@
 # Monte Carlo - Coins Flips
 
-**Simulations**
+# Simulations
 
-- [X] Coin Flips
-- [ ] Experiments 
-- [ ] Quaterly Holdback
+**Coin Flips (Completed)**
+
+Run n simulations of m coin flips. Calculate mean and standard deviation. 
+This is the monte carlo simulation for the binomial distribution.
+
+**Experiments (in progress)**
+
+- What is the true/false positive rate of our early peaking calls for a given 
+  experiment
+- If we decide to make the early peaking call, what should be the pvalue accepted?
+
+
+**Quaterly Holdback**
+
+
+
+**Friendship Paradox**
+
+- In a social network, why do your friend are more likely to have more friends than you 
+- What if the number of friends follows: 
+  (1) normal distribution
+  (2) exponential
+  (3) logarithmic
+
+**pvalue Optimization**
+
 
---
+++ MonteCarloCoins/experiments.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+
+def experiment():
+    """
+    Compute daily power, pvalue, ... until experiment reach full power
+
+    Parameters
+    ----------
+    > daily_traffic: tuple(float, float)
+        mean and variance of daily traffic for an experiment
+    > n: sample size needed to achieve power
+    > abs_delta: 
+    > 
+    """
+    pass
+    
+
+
+def main():
+    """
+    We consider three situations for a one-sided positive test
+    - positive difference is statistically significative
+    - positive difference is not statistically significative
+    - negative difference is statistically significative
+
+    1. We want to calculate the risk ie true/false positive rate if we decide 
+    to call our experiments using early peaking.
+
+    2. If we want to make the early peaking call, what should be the pvalue?
+
+
+    """
+    pass
+    
+
+
+if __name__ == "__main__":
+    main()
