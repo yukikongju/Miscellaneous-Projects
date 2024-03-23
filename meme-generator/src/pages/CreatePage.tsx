24b50b64d090cb4b85bb7f6ac7cac1cb07111541
yukikongju <emulie.chhor@hotmail.com>
2024-03-22 22:18:25 -0400

    init download and save button + center meme

--- meme-generator/src/pages/CreatePage.tsx
+++ meme-generator/src/pages/CreatePage.tsx
@@ -32,7 +32,7 @@ export default function CreatePage() {
 
   return (
     <main>
-      <div className="mt-3 mb-3 overflow-y-scroll h-60">
+      <div className="mt-3 mb-3 overflow-y-scroll h-60 flex justify-center">
         <div className="grid grid-cols-10 gap-1">
           {memeTemplates.map((item, index) => (
             <MemeCard
@@ -43,26 +43,34 @@ export default function CreatePage() {
           ))}
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-2 my-3">
-        <input
-          type="text"
-          placeholder="Top Text"
-          name="topText"
-          value={meme.topText}
-          onChange={handleMeme}
-        />
-        <input
-          type="text"
-          placeholder="Bottom Text"
-          name="bottomText"
-          value={meme.bottomText}
-          onChange={handleMeme}
-        />
-      </div>
-      <div className="ml-10 mr-10 p-1 border-black border-2 flex items-center justify-center relative">
-        <img src={meme.imageUrl} className="inset-0 h-full w-full" />
-        <h2 className="meme--text absolute top-0">{meme.topText}</h2>
-        <h2 className="meme--text absolute bottom-0">{meme.bottomText}</h2>
+      <div className="">
+        <div className="grid grid-cols-2 gap-2 my-3">
+          <input
+            type="text"
+            placeholder="Top Text"
+            name="topText"
+            value={meme.topText}
+            onChange={handleMeme}
+            className="meme--textbox"
+          />
+          <input
+            type="text"
+            placeholder="Bottom Text"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleMeme}
+            className="meme--textbox"
+          />
+        </div>
+        <div className="mx-auto p-1 border-black border-2 flex items-center justify-center relative h-[450px] w-[450px]">
+          <img src={meme.imageUrl} className="inset-0 h-full w-full" />
+          <h2 className="meme--text absolute top-0">{meme.topText}</h2>
+          <h2 className="meme--text absolute bottom-0">{meme.bottomText}</h2>
+        </div>
+        <div className="">
+          <button className="meme--button">Download Meme</button>
+          <button className="meme--button">Save Meme</button>
+        </div>
       </div>
     </main>
   );
