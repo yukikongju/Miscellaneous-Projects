68b83d4839ff4dd8dd9d21108be3c0ff2017eafc
yukikongju <emulie.chhor@hotmail.com>
2024-04-03 18:19:52 -0400

    fzf menu if grades/method is not set

--- CS344/p1/comp.sh
+++ CS344/p1/comp.sh
@@ -88,71 +88,74 @@ function usage {
 }
 
 
-function menu {
-    # --- Select Menu: (1) computation method: mean/median; (2) gradestype: student/exam
+file_name="grades.txt"
+# usage
+
+# --- process flags
+n=$# # number of parameters
+args=("$@") # store args in array
+i=0
+while [ $i -lt $n ]; do
+    param=${args[$i]}
+    case $param in
+	-f | --file) # read file_name
+	    if [ $((i+1)) -lt $n ]; then
+		file_name="${args[i+1]}"
+		((i++))
+	    else
+		echo "Usage: $0 -f <FILE_NAME>.txt"
+	    fi
+	    ;;
+	-g | --grade) # select grade type: "student" or "exam"
+	    if [ $((i+1)) -le $n ]; then
+		selected_grade_type="${args[i+1]}"
+		((i++))
+	    else
+		echo "Usage: $0 -g <GRADE>. Values: 'student', 'exam' "
+	    fi
+	    ;;
+	-m | --method) # select computation method: "mean" or "median"
+	    if [ $((i+1)) -le $n ]; then
+		selected_computation_method="${args[i+1]}"
+		((i++))
+	    else
+		echo "Usage: $0 -m <METHOD>. Values: 'mean', 'median' "
+	    fi
+	    ;;
+	-v | --verbose)
+	    # set -x
+	    verbose=1
+	    ;;
+	*) echo "Unrecognized flag";;
+    esac
+
+    ((i++))
+done
+
+# --- check if variables are set. if not, then ask with menu
+if [ -z "$selected_computation_method" ]; then
     computation_methods=("mean" "median")
     selected_computation_method=$(printf '%s\n' "${computation_methods[@]}" | fzf --reverse --with-nth 1 -d "\t" --header "Select a computation method:")
+fi
 
-    # student: row-wise; exam: column-wise
+if [ -z "$selected_grade_type" ]; then
     grade_types=("student" "exam")
     selected_grade_type=$(printf '%s\n' "${grade_types[@]}" |fzf --reverse --with-nth 1 -d "\t" --header "Select a grade type:")
+fi
 
-    # --- Compute based on computation method and grade type
-    compute_results $file_name $selected_computation_method $selected_grade_type
-}
+# --- Compute based on computation method and grade type
+compute_results $file_name $selected_computation_method $selected_grade_type
 
-# --- test cases
+# ---------------------------- TESTS --------------------
+
+# --- test cases: set 1
 # compute_results "grades.txt" "mean" "student" # okay
 # compute_results "grades.txt" "mean" "exam" # okay
 # compute_results "grades.txt" "median" "student" # okay
 # compute_results "grades.txt" "median" "exam" # okay
 
-file_name="grades.txt"
-# menu
-# usage
-
-if [ $# -eq 0 ]; then
-    menu
-else
-    # - process flags
-    n=$# # number of parameters
-    args=("$@") # store args in array
-    i=0
-    while [ $i -lt $n ]; do
-	# param="${!i}"
-	param=${args[$i]}
-	case $param in
-	    -f | --file) # read file_name
-		if [ $((i+1)) -lt $n ]; then
-		    file_name="${args[i+1]}"
-		    ((i++))
-		else
-		    echo "Usage: $0 -f <FILE_NAME>.txt"
-		fi
-		;;
-	    -g | --grade) # select grade type: "student" or "exam"
-		if [ $((i+1)) -le $n ]; then
-		    selected_grade_type="${args[i+1]}"
-		    ((i++))
-		else
-		    echo "Usage: $0 -g <GRADE>. Values: 'student', 'exam' "
-		fi
-		;;
-	    -m | --method) # select computation method: "mean" or "median"
-		if [ $((i+1)) -le $n ]; then
-		    selected_computation_method="${args[i+1]}"
-		    ((i++))
-		else
-		    echo "Usage: $0 -m <METHOD>. Values: 'mean', 'median' "
-		fi
-		;;
-	    -v | --verbose)
-		;;
-	    *) echo "Unrecognized flag";;
-	esac
-
-	((i++))
-    done
-fi
-
-
+# --- test cases: set 2
+# ./comp.sh -g "student" -m "mean" # okay
+# ./comp.sh -g "student" -m "median" # okay
+# ./comp.sh -g "exam" -m "mean"  # okay
+# ./comp.sh -g "exam" -m "median" # okay
