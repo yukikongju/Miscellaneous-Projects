77eb7bc34bbd737e9c33233eda0fde9bb59b66bf
yukikongju <emulie.chhor@hotmail.com>
2024-04-02 23:37:29 -0400

    compute median

--- CS344/p1/comp.sh
+++ CS344/p1/comp.sh
@@ -10,16 +10,37 @@ function mean {
 
 function median {
     list=$1
+    n=$(echo $list | wc -w)
+    sorted_list=$( echo $list | tr ' ' '\n' | sort -g | tr '\n' ' ' )
+    echo $sorted_list
+
+    # compute median
+    if [ $((n % 2)) -eq 0 ]; then
+	k=$((n/2))
+	m0=$(echo "$sorted_list" | awk -v i="$k" '{print $i}')
+	m1=$(echo "$sorted_list" | awk -v i="$((k+1))" '{print $i}')
+	med=$(( (m0 + m1) / 2 ))
+    else
+	k=$((n/2 +1))
+	med=$(echo "$sorted_list" | awk -v k="$k" '{print $k}')
+    fi
+
+    # return median
+    echo $med
 }
 
 function read_grades_file {
     file_name=$1
-    # grade_type=$2
+    computation_method=$2
+    grade_type=$3
 
     # - reading row by row
     while read line; do
 	# echo $line | tr " "  "\n"
-	echo $line
+	# echo $line
+	# s=$( echo $line | tr " " "\n" | sort -g )
+	# echo $s
+	$computation_method "$line"
     done < $file_name
 
     # - reading col by col
@@ -28,7 +49,7 @@ function read_grades_file {
     while [ $i -lt $num_cols ]; do
 	# col=$( cut -f $i $file_name | tr " " "\n" | sort -g )
 	col=$( cut -f $i $file_name | sort -g )
-	echo $col
+	# echo $col
 	((i++))
     done
 
@@ -53,7 +74,8 @@ function main {
 
 }
 
-read_grades_file "grades.txt"
+# read_grades_file "grades.txt" "mean" "student"
+read_grades_file "grades.txt" "median" "student"
 # main
 
 
