b9a7b62da27aaa3b2063b39ebf469e19e390c253
yukikongju <emulie.chhor@hotmail.com>
2024-08-14 22:16:26 -0400

    updated user marker position on map click

--- MontrealBixiViz/notes.md
+++ MontrealBixiViz/notes.md
@@ -3,9 +3,10 @@ Steps:
     - [ ] Map of Montreal
     - [X] [Map of Bixi](https://gbfs.velobixi.com/gbfs/gbfs.json)
     - [X] [Map of REV](https://donnees.montreal.ca/en/dataset/pistes-cyclables/resource/0dc6612a-be66-406b-b2d9-59c9e1c65ebf)
-- [ ] Drop user on the map or enter address to move user
+- [X] Update user position on map click
 - [ ] Add Visuals
     - [ ] Add pins for closest bixi that are disponible (green dispo; red not)
+    - [ ] Add user popup with nearest BIXI spot by walk
     - [ ] ~~Add pins for saved spot~~
 
 
--- MontrealBixiViz/src/scripts/app.js
+++ MontrealBixiViz/src/scripts/app.js
@@ -3,11 +3,18 @@ class Coordinate {
     this.x = x;
     this.y = y;
   }
+
+  updatePosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
+// initialize variables
 var popup = L.popup();
 const coord = new Coordinate(45.5335, -73.6483); // montreal coordinates
 var map = L.map("map").setView([coord.x, coord.y], 13);
+var marker = L.marker([coord.x, coord.y]).addTo(map).bindPopup("You are here"); // .openPopup();
 
 function loadMap() {
   // Step 3: Add a tile layer to the map
@@ -16,16 +23,18 @@ function loadMap() {
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
+}
 
-  // Step 4: Optionally add a marker
-  L.marker([coord.x, coord.y]).addTo(map).bindPopup("Montreal, QC"); // .openPopup();
+function updateMarkerPosition(e) {
+  coord.updatePosition(e.latlng.lat, e.latlng.lng);
+
+  // TODO: set popup content to see closest Bixi stations
+  var popupContent = "Clicked here!";
+  marker.setLatLng(e.latlng).setPopupContent(popupContent).openOn(map);
 }
 
 function onMapClick(e) {
-  popup
-    .setLatLng(e.latlng)
-    .setContent("Clicked " + e.latlng.toString())
-    .openOn(map);
+  updateMarkerPosition(e);
 }
 
 function main() {
