934d051c6749a0aef2ebeaca838ee8d185b195ed
yukikongju <emulie.chhor@hotmail.com>
2024-08-16 13:21:57 -0400

    initialize vite

---
+++ MontrealBixiViz/src/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "./", // Set this to your project root
+  build: {
+    outDir: "dist", // Output directory for production build
+  },
+});
