1ba18d86e806fc5459c2d3af4be42e8fea9aa388
yukikongju <emulie.chhor@hotmail.com>
2025-03-19 11:58:06 -0400

    init taxis

---
+++ NYTaxisTrip/README.md
@@ -0,0 +1,13 @@
+# NY Taxis Trip
+
+This project is a replica of the [DE-zoomcamp]. We to learn the following:
+1. Containerization w/ Docker & Terraform => make build, deploy, test applications reproducible and quick
+2. Data Orchestration w/ Airflow =>
+3. Data Warehouse w/ BigQuery (skipped since I have used BQ previously)
+4. Analytics Engineering w/ dbt =>
+5. Batch Processing w/ spark =>
+6. Batch Streaming w/ kafka =>
+
+## Resources
+
+- [TLC Trip Record Data](https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page)
---
+++ NYTaxisTrip/ingest_data.py
@@ -0,0 +1,70 @@
+import argparse
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+from time import time
+
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def main(args):
+    # --- parse argument
+    user = args.user
+    password = args.password
+    host = args.host
+    port = args.port
+    db = args.db
+    table_name = args.table_name
+    url = args.url
+
+    # --- get parquet file from url
+    print("Reading parquet file")
+    csv_name = "output.csv"
+    df = pd.read_parquet(url)
+    df.to_csv(csv_name, index=False)
+    #  os.system(f"wget {url} -O {csv_name}")
+
+    # --- create db connection
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+
+    # --- iteratively push batches of data to database
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
+    df.to_sql(name=table_name, con=engine, if_exists="append")
+
+    while True:
+        try:
+            t_start = time()
+
+            df = next(df_iter)
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            df.to_sql(name=table_name, con=engine, if_exists="append")
+
+            t_end = time()
+            print("inserted another chunk, took %.3f second" % (t_end - t_start))
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Ingest parquet file to postgres")
+
+    parser.add_argument("--user", required=True, help="username for postgres")
+    parser.add_argument("--password", required=True, help="password for postgres")
+    parser.add_argument("--host", required=True, help="host for postgres")
+    parser.add_argument("--port", required=True, help="port for postgres")
+    parser.add_argument("--db", required=True, help="database for postgres")
+    parser.add_argument("--table_name", required=True, help="table name for postgres")
+    parser.add_argument("--url", required=True, help="URL of the parquet file")
+
+    args = parser.parse_args()
+    main(args)
