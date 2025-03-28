342e3c42ff1344824587953976121eea1be6e105
yukikongju <emulie.chhor@hotmail.com>
2025-03-28 14:56:25 -0400

    added staging for yellow taxis table

---
+++ NYTaxisTrip/dbt/models/staging/stg_yellow_taxis.sql
@@ -0,0 +1,29 @@
+{{ config(materialized='table') }}
+
+with yellow_taxis as (
+    SELECT
+	"VendorID" as vendor_id,
+	"tpep_pickup_datetime",
+	"tpep_dropoff_datetime",
+	"passenger_count",
+	"trip_distance",
+	"RatecodeID" as rate_code_id,
+	"store_and_fwd_flag",
+	"PULocationID" as pu_location_id,
+	"DOLocationID" as do_location_id,
+	"payment_type",
+	"fare_amount",
+	"extra",
+	"mta_tax",
+	"tip_amount",
+	"tolls_amount",
+	"improvement_surcharge",
+	"total_amount",
+	"congestion_surcharge",
+	"Airport_fee" as airport_fee
+	--  "cbd_congestion_fee"
+    FROM public.yellow_taxi_trips
+    ORDER BY tpep_pickup_datetime DESC
+)
+
+select * from yellow_taxis
