f5cedfefdae3d22d0bc548af08b85347fa81d94b
yukikongju <emulie.chhor@hotmail.com>
2025-04-07 22:46:59 -0400

    updated fact table to have surrogate key

--- NYTaxisTrip/dbt/models/marts/fct_trips.sql
+++ NYTaxisTrip/dbt/models/marts/fct_trips.sql
@@ -10,54 +10,64 @@ with yellow_taxis as (
     from {{ ref('stg_green_taxis') }}
 ), taxis as (
     select
-	taxi_type_id,
-	vendor_id,
-	pickup_datetime,
-	pickup_date,
-	pickup_time,
-	dropoff_datetime,
-	dropoff_date,
-	dropoff_time,
-	pu_location_id,
-	do_location_id,
-	rate_code_id,
-	payment_id,
-	passenger_count,
-	trip_distance,
-	fare_amount
-	mta_tax,
-	extra,
-	tip_amount,
-	improvement_surcharge,
-	congestion_surcharge,
-	total_amount
+        taxi_type_id,
+        vendor_id,
+        pickup_datetime,
+        pickup_date,
+        pickup_time,
+        dropoff_datetime,
+        dropoff_date,
+        dropoff_time,
+        pu_location_id,
+        do_location_id,
+        rate_code_id,
+        payment_id,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        mta_tax,
+        extra,
+        tip_amount,
+        improvement_surcharge,
+        congestion_surcharge,
+        total_amount
     from yellow_taxis
     union all
     select
-	taxi_type_id,
-	vendor_id,
-	pickup_datetime,
-	pickup_date,
-	pickup_time,
-	dropoff_datetime,
-	dropoff_date,
-	dropoff_time,
-	pu_location_id,
-	do_location_id,
-	rate_code_id,
-	payment_id,
-	passenger_count,
-	trip_distance,
-	fare_amount
-	mta_tax,
-	extra,
-	tip_amount,
-	improvement_surcharge,
-	congestion_surcharge,
-	total_amount
+        taxi_type_id,
+        vendor_id,
+        pickup_datetime,
+        pickup_date,
+        pickup_time,
+        dropoff_datetime,
+        dropoff_date,
+        dropoff_time,
+        pu_location_id,
+        do_location_id,
+        rate_code_id,
+        payment_id,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        mta_tax,
+        extra,
+        tip_amount,
+        improvement_surcharge,
+        congestion_surcharge,
+        total_amount
     from green_taxis
 )
 
 select
-
+    {{ dbt_utils.generate_surrogate_key([
+        'taxi_type_id',
+        'vendor_id',
+        'pickup_date',
+        'pickup_time',
+        'dropoff_date',
+        'dropoff_time',
+        'pu_location_id',
+        'do_location_id'
+    ]) }} as trip_id,
+    *
 from taxis
