3b5d6d4ae1f30c1cc009140e95a215ea408163ed
yukikongju <emulie.chhor@hotmail.com>
2025-05-30 12:54:25 -0400

    added lenet model test

--- PapersFromScratch/tests/test_lenet.py
+++ PapersFromScratch/tests/test_lenet.py
@@ -7,6 +7,7 @@ from models.lenet.lenet import LeNet, Block
 @pytest.fixture
 def tensor() -> torch.Tensor:
     return torch.randint(1, 255, size=(5, 1, 32, 32)).float()
+    #  return torch.randint(1, 255, size=(5, 1, 64, 64)).float()
 
 
 def test_block(tensor: torch.Tensor):
@@ -19,11 +20,21 @@ def test_block(tensor: torch.Tensor):
     padding = block.layer[0].padding[0]
     dilation = block.layer[0].dilation[0]
 
-    H_conv = (H + 2*padding - dilation*(kernel_size - 1) - 1) // stride + 1
-    W_conv = (W + 2*padding - dilation*(kernel_size - 1) - 1) // stride + 1
+    H_conv = (H + 2 * padding - dilation * (kernel_size - 1) - 1) // stride + 1
+    W_conv = (W + 2 * padding - dilation * (kernel_size - 1) - 1) // stride + 1
     H_OUTPUT = H_conv // 2
     W_OUTPUT = W_conv // 2
 
     #  assert output.size() == (B, OUTPUT_CHANNELS, 14, 14)
     assert output.size() == (B, OUTPUT_CHANNELS, H_OUTPUT, W_OUTPUT)
 
+
+def test_lenet(tensor):
+    B, C, H, W = tensor.size()
+    conv_features = [6, 16]
+    fc_features = [400, 120, 84]
+
+    NUM_CLASSES = 10
+    lenet = LeNet(C, NUM_CLASSES, conv_features, fc_features)
+    output = lenet(tensor)
+    assert output.size() == (B, NUM_CLASSES)
