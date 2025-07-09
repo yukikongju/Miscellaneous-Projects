19aa7b16216934f2ad92fd717c09150be71cee1b
yukikongju <emulie.chhor@hotmail.com>
2025-07-09 15:31:06 -0400

    added additional columns

--- LTVPredictions/tables/users_hau_daily.sql
+++ LTVPredictions/tables/users_hau_daily.sql
@@ -1,9 +1,12 @@
-create table `relax-melodies-android.late_conversions.users_hau_daily` (
+create or replace table `relax-melodies-android.late_conversions.users_hau_daily` (
     event_date DATE,
     user_id STRING,
     user_pseudo_id STRING,
     platform STRING,
     country STRING,
+    traffic_source_name STRING,
+    traffic_source_medium STRING,
+    traffic_source STRING,
     hau STRING
 )
 PARTITION BY
--- LTVPredictions/tables/users_utm_daily.sql
+++ LTVPredictions/tables/users_utm_daily.sql
@@ -1,10 +1,17 @@
-create table `relax-melodies-android.late_conversions.users_utm_daily` (
+create or replace table `relax-melodies-android.late_conversions.users_utm_daily` (
     event_date DATE,
     user_id STRING,
     user_pseudo_id STRING,
     platform STRING,
     country STRING,
-    utm STRING
+    traffic_source_name STRING,
+    traffic_source_medium STRING,
+    traffic_source STRING,
+    campaign STRING,
+    utm_source STRING,
+    page_title STRING,
+    page_location STRING,
+    page_referrer STRING,
 )
 PARTITION BY
     event_date
