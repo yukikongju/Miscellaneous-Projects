8dedcaa6f15dad57866e18fcd2076a1348c47da1
yukikongju <emulie.chhor@hotmail.com>
2025-07-09 15:06:57 -0400

    added partition and cluster in users hau/utm daily

--- LTVPredictions/tables/users_hau_daily.sql
+++ LTVPredictions/tables/users_hau_daily.sql
@@ -1,8 +1,12 @@
 create table `relax-melodies-android.late_conversions.users_hau_daily` (
-    event_date TIMESTAMP,
+    event_date DATE,
     user_id STRING,
     user_pseudo_id STRING,
     platform STRING,
     country STRING,
-    hau STRING,
+    hau STRING
 )
+PARTITION BY
+    event_date
+CLUSTER BY
+    user_id, user_pseudo_id, platform, country;
