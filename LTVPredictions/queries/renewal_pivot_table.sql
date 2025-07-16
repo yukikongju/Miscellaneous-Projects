43051de0b2f957d7ab03eb628c824805d891fd77
yukikongju <emulie.chhor@hotmail.com>
2025-07-16 11:20:49 -0400

    added mature cohorts to renewal pivot query

--- LTVPredictions/queries/renewal_pivot_table.sql
+++ LTVPredictions/queries/renewal_pivot_table.sql
@@ -1,114 +1,147 @@
 -- `late_conversions.renewal_rates_cohorted`
 -- `late_conversions.renewal_proceeds_cohorted`
 
-declare start_date string default "2020-01-01";
-declare end_date string default "2025-07-01";
-
-with paid_data as (
+DECLARE
+  start_date string DEFAULT "2020-01-01";
+DECLARE
+  end_date string DEFAULT "2025-07-01";
+WITH
+  paid_data AS (
   SELECT
     user_id,
     event_name,
     platform,
-    event_timestamp_s as paid_timestamp_s,
+    event_timestamp_s AS paid_timestamp_s,
     ep.value.float_value AS proceeds,
-    row_number() over (partition by user_id, event_name, platform order by event_timestamp_s desc) as rn
-  FROM `relax-melodies-android.backend.events`,
-    UNNEST(event_params) as ep
+    ROW_NUMBER() OVER (PARTITION BY user_id, event_name, platform ORDER BY event_timestamp_s DESC) AS rn
+  FROM
+    `relax-melodies-android.backend.events`,
+    UNNEST(event_params) AS ep
   WHERE
     TIMESTAMP_TRUNC(event_timestamp_s, DAY) >= TIMESTAMP(start_date)
-      and TIMESTAMP_TRUNC(event_timestamp_s, DAY) <= TIMESTAMP(end_date)
-    and event_name = 'subscription_start_paid'
-    and user_id is not null
-    and ep.key = 'converted_procceds'
-), paid_unique as (
-  select
+    AND TIMESTAMP_TRUNC(event_timestamp_s, DAY) <= TIMESTAMP(end_date)
+    AND event_name = 'subscription_start_paid'
+    AND user_id IS NOT NULL
+    AND ep.key = 'converted_procceds' ),
+  paid_unique AS (
+  SELECT
     user_id,
     event_name,
     platform,
     proceeds,
     paid_timestamp_s
-  from paid_data
-  where rn = 1
-), renewal_data as (
+  FROM
+    paid_data
+  WHERE
+    rn = 1 ),
+  renewal_data AS (
   SELECT
     user_id,
     event_name,
     platform,
-    case when ep.key = 'converted_procceds' then ep.value.float_value end AS proceeds,
-    case when ep.key = 'feature_id' then ep.value.string_value end as renewal_sku,
-    event_timestamp_s as renewal_timestamp_s,
-    lag(event_timestamp_s) over (partition by user_id order by event_timestamp_s) as prev_renewal_timestamp_s
-  FROM `relax-melodies-android.backend.events`,
-    UNNEST(event_params) as ep
+    CASE
+      WHEN ep.key = 'converted_procceds' THEN ep.value.float_value
+  END
+    AS proceeds,
+    CASE
+      WHEN ep.key = 'feature_id' THEN ep.value.string_value
+  END
+    AS renewal_sku,
+    event_timestamp_s AS renewal_timestamp_s,
+    LAG(event_timestamp_s) OVER (PARTITION BY user_id ORDER BY event_timestamp_s) AS prev_renewal_timestamp_s
+  FROM
+    `relax-melodies-android.backend.events`,
+    UNNEST(event_params) AS ep
   WHERE
     TIMESTAMP_TRUNC(event_timestamp_s, DAY) >= TIMESTAMP(start_date)
-      and TIMESTAMP_TRUNC(event_timestamp_s, DAY) <= TIMESTAMP(end_date)
-    and event_name = 'subscription_renew_paid'
-    and (user_id is not null or user_id != '')
-    and ep.key in ('converted_procceds', 'feature_id')
-), renewal_unique as ( -- remove renewal processed twice
+    AND TIMESTAMP_TRUNC(event_timestamp_s, DAY) <= TIMESTAMP(end_date)
+    AND event_name = 'subscription_renew_paid'
+    AND (user_id IS NOT NULL
+      OR user_id != '')
+    AND ep.key IN ('converted_procceds',
+      'feature_id') ),
+  renewal_unique AS ( -- remove renewal processed twice
   SELECT
     user_id,
     event_name,
     platform,
     proceeds,
     renewal_timestamp_s
-  FROM renewal_data
+  FROM
+    renewal_data
   WHERE
-    prev_renewal_timestamp_s is NULL or TIMESTAMP_SUB(renewal_timestamp_s, INTERVAL 1 DAY) > prev_renewal_timestamp_s
-    and renewal_sku like '%year%' --- only keep yearly sku
-  ORDER BY user_id, renewal_timestamp_s
-), joined_table as (
-    SELECT
-      p.user_id,
-      p.platform,
-      format_timestamp('%Y-%m', p.paid_timestamp_s) as paid_year_month,
-      p.paid_timestamp_s,
-      r.renewal_timestamp_s,
-      p.proceeds as paid_proceeds,
-      r.proceeds as renewal_proceeds,
-      DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) as days_to_renewal,
-      case
-        when DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) <= 31 then '1-Month'
-        when DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) between 31 and 60 then '2-Months'
-        when DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) between 60 and 180 then 'Half-Year'
-        when DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) between 180 and 365 then '1-Year'
-        when DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) between 365 and 730 then '2-Years'
-        when DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) between 730 and 1095 then '3-Years'
-        when DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) between 1095 and 1460 then '4-Years'
-        when DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) between 1460 and 1825 then '5-Years'
-        else '>5-Years'
-      end as renewal_bucket
-  FROM paid_unique as p
-    LEFT JOIN renewal_data r on p.user_id = r.user_id
-  ORDER BY user_id, days_to_renewal
-), paid_cohorts as (
-  select
+    prev_renewal_timestamp_s IS NULL
+    OR TIMESTAMP_SUB(renewal_timestamp_s, INTERVAL 1 DAY) > prev_renewal_timestamp_s
+    AND renewal_sku LIKE '%year%' --- only keep yearly sku
+  ORDER BY
+    user_id,
+    renewal_timestamp_s ),
+  joined_table AS (
+  SELECT
+    p.user_id,
+    p.platform,
+    FORMAT_TIMESTAMP('%Y-%m', p.paid_timestamp_s) AS paid_year_month,
+    p.paid_timestamp_s,
+    r.renewal_timestamp_s,
+    p.proceeds AS paid_proceeds,
+    r.proceeds AS renewal_proceeds,
+    DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) AS days_to_renewal,
+    CASE
+      WHEN DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) <= 31 THEN '1-Month'
+      WHEN DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) BETWEEN 31
+    AND 60 THEN '2-Months'
+      WHEN DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) BETWEEN 60 AND 180 THEN 'Half-Year'
+      WHEN DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) BETWEEN 180
+    AND 365 THEN '1-Year'
+      WHEN DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) BETWEEN 365 AND 730 THEN '2-Years'
+      WHEN DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) BETWEEN 730
+    AND 1095 THEN '3-Years'
+      WHEN DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) BETWEEN 1095 AND 1460 THEN '4-Years'
+      WHEN DATE_DIFF(r.renewal_timestamp_s, p.paid_timestamp_s, DAY) BETWEEN 1460
+    AND 1825 THEN '5-Years'
+      ELSE '>5-Years'
+  END
+    AS renewal_bucket
+  FROM
+    paid_unique AS p
+  LEFT JOIN
+    renewal_data r
+  ON
+    p.user_id = r.user_id
+  ORDER BY
+    user_id,
+    days_to_renewal ),
+  paid_cohorts AS (
+  SELECT
     paid_year_month,
     platform,
-    count(*) as num_paid,
-    avg(paid_proceeds) as paid_proceeds
-  from joined_table
-  group by
+    COUNT(*) AS num_paid,
+    AVG(paid_proceeds) AS paid_proceeds
+  FROM
+    joined_table
+  GROUP BY
     paid_year_month,
-    platform
-), renewal_cohorts as (
-  select
+    platform ),
+  renewal_cohorts AS (
+  SELECT
     paid_year_month,
     platform,
     renewal_bucket,
-    count(*) as num_renewals,
-    avg(renewal_proceeds) as renewal_proceeds
-  from joined_table
-  where
-    renewal_timestamp_s is not null
-    and renewal_bucket not in ('1-Month', '2-Months', 'Half-Year')
-  group by
+    COUNT(*) AS num_renewals,
+    AVG(renewal_proceeds) AS renewal_proceeds
+  FROM
+    joined_table
+  WHERE
+    renewal_timestamp_s IS NOT NULL
+    AND renewal_bucket NOT IN ('1-Month',
+      '2-Months',
+      'Half-Year')
+  GROUP BY
     paid_year_month,
     platform,
-    renewal_bucket
-), aggregate_cohorts as (
-  select
+    renewal_bucket ),
+  aggregate_cohorts AS (
+  SELECT
     r.paid_year_month,
     r.platform,
     r.renewal_bucket,
@@ -116,42 +149,74 @@ with paid_data as (
     p.paid_proceeds,
     p.num_paid,
     r.num_renewals,
-    round(r.num_renewals / p.num_paid * 100.0, 4) as renewal_percentage
-  from renewal_cohorts as r
-  left join paid_cohorts as p
-  on r.paid_year_month = p.paid_year_month and r.platform = p.platform
-)
+    ROUND(r.num_renewals / p.num_paid * 100.0, 4) AS renewal_percentage
+  FROM
+    renewal_cohorts AS r
+  LEFT JOIN
+    paid_cohorts AS p
+  ON
+    r.paid_year_month = p.paid_year_month
+    AND r.platform = p.platform ),
+  mature_cohorts AS (
+  SELECT
+    paid_year_month,
+    platform,
+    renewal_bucket,
+    renewal_proceeds,
+    paid_proceeds,
+    num_paid,
+    num_renewals,
+    renewal_percentage
+  FROM
+    aggregate_cohorts
+  WHERE (
+      (renewal_bucket = '1-Year'AND paid_year_month < FORMAT_DATE('%Y-%m', DATE_SUB(CURRENT_DATE(), INTERVAL 1 year))) OR
+      (renewal_bucket = '2-Years'AND paid_year_month < FORMAT_DATE('%Y-%m', DATE_SUB(CURRENT_DATE(), INTERVAL 2 year))) OR
+
+      (renewal_bucket = '3-Years'AND paid_year_month < FORMAT_DATE('%Y-%m', DATE_SUB(CURRENT_DATE(), INTERVAL 3 year))) OR
+      (renewal_bucket = '4-Years'AND paid_year_month < FORMAT_DATE('%Y-%m', DATE_SUB(CURRENT_DATE(), INTERVAL 4 year))) OR
+      (renewal_bucket = '5-Years'AND paid_year_month < FORMAT_DATE('%Y-%m', DATE_SUB(CURRENT_DATE(), INTERVAL 5 year)))
+  )
+  )
+
+
 
 --- for "renewal_percentage"
-select
+SELECT
   *
-from (
-  select
+FROM (
+  SELECT
     paid_year_month,
     platform,
     renewal_bucket,
     renewal_percentage
-  from aggregate_cohorts
-)
-pivot (
-  avg(renewal_percentage)
-  FOR renewal_bucket in ('1-Year', '2-Years', '3-Years', '4-Years', '5-Years', '>5-Years')
-)
-order by paid_year_month, platform
+  FROM
+    mature_cohorts )
+PIVOT
+  ( AVG(renewal_percentage) FOR renewal_bucket IN ('1-Year',
+      '2-Years',
+      '3-Years',
+      '4-Years',
+      '5-Years',
+      '>5-Years') )
+ORDER BY
+  paid_year_month,
+  platform
+
 
 --- for "renewal_proceeds"
 select
-  *
+*
 from (
-  select
-    paid_year_month,
-    platform,
-    renewal_bucket,
-    renewal_proceeds
-  from aggregate_cohorts
+select
+ paid_year_month,
+ platform,
+ renewal_bucket,
+ renewal_proceeds
+from mature_cohorts
 )
 pivot (
-  avg(renewal_proceeds)
-  FOR renewal_bucket in ('1-Year', '2-Years', '3-Years', '4-Years', '5-Years', '>5-Years')
+avg(renewal_proceeds)
+FOR renewal_bucket in ('1-Year', '2-Years', '3-Years', '4-Years', '5-Years', '>5-Years')
 )
 order by paid_year_month, platform
