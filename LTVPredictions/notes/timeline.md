af9199389352b7cfa83fb1ae99a60fa6c7f04ee5
yukikongju <emulie.chhor@hotmail.com>
2025-07-29 13:03:10 -0400

    added query for final table with late conversions

--- LTVPredictions/notes/timeline.md
+++ LTVPredictions/notes/timeline.md
@@ -1,3 +1,11 @@
 Jul 24, 2025
 - backfilled `users_network_attribution` from 2023-01-01 to 2025-07-22
     * need to remove from 07-22 because unmature yet
+
+Jul 29, 2025
+- Created `latest_renewal_rates`
+- Created `renewal_rates_ma_imputed` and `renewal_proceeds_ma_imputed`, which
+  compute the rates and proceeds from the latest renewal and ensure that
+  no cells are empty. Logic:
+    * For Renewal Rates:
+    * For Proceeds: depreciation rate 0.8
---
+++ LTVPredictions/queries/final_table_with_late_conversions.sql
@@ -0,0 +1,32 @@
+--- cost: 1.62 GB (final table is unpartitioned)
+declare start_date datetime default '2025-06-01';
+declare end_date datetime default '2025-06-07';
+
+select
+  f.*,
+  f.paid * r.`1-Year` * p.`1-Year` as renewal_rev_1Year,
+  f.paid * r.`2-Years` * p.`2-Years` as renewal_rev_2Years,
+  f.paid * r.`3-Years` * p.`3-Years` as renewal_rev_3Years,
+  f.revenue + f.paid * r.`1-Year` * p.`1-Year` + f.paid * r.`2-Years` * p.`2-Years` + f.paid * r.`3-Years` * p.`3-Years` as late_revenue,
+  case when f.cost_usd > 0
+    then (f.revenue + f.paid * r.`1-Year` * p.`1-Year` + f.paid * r.`2-Years` * p.`2-Years` + f.paid * r.`3-Years` * p.`3-Years`) / f. cost_usd
+    else null
+  end as late_roas,
+  case when f.cost_usd > 0
+    then f.revenue / f.cost_usd
+    else null
+  end as roas
+from `relax-melodies-android.ua_dashboard_prod.final_table` f
+left join relax-melodies-android.late_conversions.renewal_rates_ma_imputed r
+on
+  f.network = r.network
+  and f.platform = r.platform
+  and f.country = r.country_code
+left join `relax-melodies-android.late_conversions.renewal_proceeds_ma_imputed` p
+on
+  f.network = p.network
+  and f.platform = p.platform
+  and f.country = p.country_code
+where
+  f.date between start_date and end_date
+  and paid > 0
