7fc812ec91e913f63282da4c050fb111c34a1aa1
yukikongju <emulie.chhor@hotmail.com>
2025-08-12 11:32:12 -0400

    added incremental table for latest renewal rates cohorted

---
+++ LTVPredictions/tables/latest_renewal_rates_cohorted.sql
@@ -0,0 +1,16 @@
+create or replace table `relax-melodies-android.late_conversions.latest_renewal_rates_cohorted` (
+    year_month date,
+    network STRING,
+    platform STRING,
+    country_code STRING,
+    renewal_bucket STRING,
+    num_paid INT,
+    num_renewals INT,
+    paid_proceeds FLOAT64,
+    renewal_proceeds FLOAT64,
+    renewal_percentage FLOAT64
+)
+partition by
+    year_month
+cluster by
+    network, platform, country_code;
---
+++ LTVPredictions/tables/renewal_rates_ma_imputed_cohorted.sql
@@ -0,0 +1,11 @@
+create or replace table `relax-melodies-android.late_conversions.renewal_rates_ma_imputed_cohorted` (
+    year_month DATE,
+    network STRING,
+    platform STRING,
+    country_code STRING,
+    `1-Year` FLOAT64,
+    `2-Years` FLOAT64,
+    `3-Years` FLOAT64,
+)
+partition by year_month
+cluster by network, platform, country_code;
