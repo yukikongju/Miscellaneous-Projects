a7bb35a487ef9347f608849e462ee66083536a1e
yukikongju <emulie.chhor@hotmail.com>
2025-09-04 13:47:25 -0400

    added staging as view and incremental table

--- IncrementalTablesExample/models/staging/stg_fake_data.sql
+++ IncrementalTablesExample/models/staging/stg_fake_data.sql
@@ -1,9 +1,23 @@
-with data as (
-    select * from {{ source('raw', 'fake_data') }}
-), clean_data as (
-    select * from data
+{{
+config(
+    materialized="incremental",
+    cluster_by=["network", "platform", "country"],
+    partition_by={
+	"field": "date",
+	"data_type": "date",
+	"granularity": "day"
+    },
+    incremental_strategy = 'insert_overwrite'
+)
+}}
+
+with fake_data as (
+    select
+	*
+    from {{ source('raw', 'fake_data') }}
+    {% if is_incremental() %}
+	where loaded_timestamp > (select max(loaded_timestamp) from {{ this }})
+    {% endif %}
 )
 
-select
-    *
-from clean_data
+select * from fake_data
