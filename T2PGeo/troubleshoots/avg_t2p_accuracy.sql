a8c7d4df4182c49c715365a261f9f64d4c8147f6
yukikongju <emulie.chhor@hotmail.com>
2025-10-15 14:31:46 -0400

    added fall back model comparison

--- T2PGeo/troubleshoots/avg_t2p_accuracy.sql
+++ T2PGeo/troubleshoots/avg_t2p_accuracy.sql
@@ -1,8 +1,9 @@
-declare start_date date default '2025-06-01';
+declare start_date date default '2025-08-01';
 declare end_date date default '2025-10-01';
 declare networks default array<string> ['Apple Search Ads', 'Facebook Ads'];
 declare platforms default array<string> ['ios', 'android'];
 declare countries default array<string> ['US', 'CA', 'FR', 'MX', 'AU', 'UK', 'GB'];
+-- declare countries default array<string> ['PH', 'AR', 'MY', 'ER', 'VN']; --- countries with high trials but low paid
 declare MIN_TRIALS default 5;
 declare MIN_PAID default 2;
 
@@ -28,6 +29,13 @@ with final_table as (
     , paid
     , modeled_trial2paid as t2p
   from `ua_transform_dev.trial2paid_geobydate_model_unique`
+), t2p_less_fallback as (
+  select
+    install_date, network, platform, country
+    , trial
+    , paid
+    , modeled_trial2paid as t2p
+  from `ua_transform_dev.trial2paid_less_fallback_model`
 ), joined_table as (
   select
     f.date, f.network, f.platform, f.country
@@ -36,6 +44,7 @@ with final_table as (
     , f.t2p as t2p_actual
     , i.t2p as t2p_internal
     , g.t2p as t2p_geobydate
+    , lf.t2p as t2p_less_fallback
   from final_table f
   left join t2p_internal i
     on f.date = i.install_date
@@ -47,11 +56,17 @@ with final_table as (
       and f.network = g.network
       and f.platform = g.platform
       and f.country = g.country
+  left join t2p_less_fallback lf
+    on f.date = lf.install_date
+      and f.network = lf.network
+      and f.platform = lf.platform
+      and f.country = lf.country
 ), accuracy_table as (
   select
     network, platform, country
     , avg(safe_divide((t2p_geobydate - t2p_actual), t2p_actual)) as geobydate_mse
     , avg(safe_divide((t2p_internal - t2p_actual), t2p_actual)) as internal_mse
+    , avg(safe_divide((t2p_less_fallback - t2p_actual), t2p_actual)) as less_fallback_mse
   from joined_table
   where
     date >= start_date and date <= end_date
@@ -64,21 +79,21 @@ with final_table as (
 
 
 -- ==== ACCURACY TABLE ====
-select
-  *
-from accuracy_table
-where
-  -- date >= start_date and date <= end_date
-  network in unnest(networks)
-  and platform in unnest(platforms)
-  and country in unnest(countries)
-order by network, platform, country
-
--- ==== JOINED_TABLE ====
--- select * from joined_table
+-- select
+--   *
+-- from accuracy_table
 -- where
---   date >= start_date and date <= end_date
---   and network in unnest(networks)
+--   -- date >= start_date and date <= end_date
+--   network in unnest(networks)
 --   and platform in unnest(platforms)
 --   and country in unnest(countries)
--- order by network, platform, country, date asc
+-- order by network, platform, country
+
+-- ==== JOINED_TABLE ====
+select * from joined_table
+where
+  date >= start_date and date <= end_date
+  and network in unnest(networks)
+  and platform in unnest(platforms)
+  and country in unnest(countries)
+order by network, platform, country, date asc
