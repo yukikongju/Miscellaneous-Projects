557bead10c050965e210b8b08fccce4e1d702984
yukikongju <emulie.chhor@hotmail.com>
2025-12-02 14:13:34 -0500

    added registry example

---
+++ DesignPatternsPython/registry/README.md
@@ -0,0 +1,5 @@
+# Registry Design Pattern
+
+### Resources
+
+- [Arjan Codes - I hate long if-elif chains](https://www.youtube.com/watch?v=g7EGMWvJ1fI)
---
+++ DesignPatternsPython/registry/ex1.py
@@ -0,0 +1,46 @@
+from typing import List, Callable, Dict
+from pydantic import BaseModel
+
+
+class Document(BaseModel):
+    title: str
+    tags: List[str]
+    content: str
+
+
+def format_csv(doc: Document) -> str:
+    return f"{doc.title}.csv"
+
+
+def format_pdf(doc: Document) -> str:
+    return f"{doc.title}.pdf"
+
+
+def format_txt(doc: Document) -> str:
+    return f"{doc.title}.txt"
+
+
+FormatFn = Callable[[Document], str]
+formatters: Dict[str, FormatFn] = {
+    "csv": format_csv,
+    "pdf": format_pdf,
+    "txt": format_txt,
+}
+
+
+def get_file_name(doc: Document, format_type: str) -> str:
+    formatter = formatters.get(format_type)
+    if formatter is None:
+        raise ValueError(f"Format {format_type} not implemented")
+    return formatter(doc)
+
+
+if __name__ == "__main__":
+    doc = Document(
+        title="Title",
+        tags=["tag1", "tag2", "tag3"],
+        content="Lorem ipsum dolor sit amet",
+    )
+    print(get_file_name(doc, "txt"))
+    print(get_file_name(doc, "pdf"))
+    print(get_file_name(doc, "csv"))
