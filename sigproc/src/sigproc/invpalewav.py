a14395689ed08a9dec0d1550000d7b23253a69f8
yukikongju <emulie.chhor@hotmail.com>
2025-12-12 19:45:24 -0500

    init invpalewav

--- sigproc/src/sigproc/invpalewav.py
+++ sigproc/src/sigproc/invpalewav.py
@@ -1,4 +1,6 @@
 import argparse
+import matplotlib.pyplot as plt
+import numpy as np
 import logging
 from . import image_utils, signal_utils
 
@@ -8,9 +10,15 @@ def run():
         prog="invpalewav", description="converting .wav file to image"
     )
     parser.add_argument("-w", "--wav_path")
-    parser.add_argument("-i", "--img_path")
+    #  parser.add_argument("-i", "--img_path")
     args = parser.parse_args()
 
     logging.basicConfig(format="[%(asctime)s] %(message)s", level=logging.INFO)
 
-    print(args.img_path, args.wav_path)
+    sample_rate, signal = signal_utils.read_signal(args.wav_path)
+    n = np.sqrt(len(signal)).astype(np.int16)
+    print(n)
+    x = signal.reshape(n, -1)
+    print(x.shape)
+    plt.imshow(x)
+    plt.show()
--- sigproc/src/sigproc/signal_utils.py
+++ sigproc/src/sigproc/signal_utils.py
@@ -2,6 +2,19 @@ import logging
 import numpy as np
 from scipy.io import wavfile
 from . import image_utils
+from typing import Tuple
+
+
+def read_signal(wav_path: str) -> Tuple[int, np.ndarray]:
+    """
+    Reading signal from .wav file
+    """
+    try:
+        sample_rate, data = wavfile.read(wav_path)
+        print(sample_rate, data.shape)
+    except Exception as e:
+        raise FileNotFoundError(f"{wav_path} not found. Exiting with error {e}")
+    return sample_rate, data
 
 
 def array_to_wav(
@@ -17,5 +30,3 @@ def array_to_wav(
     wav = (amplitude * np.sin(2.0 * np.pi * frequency * arr)).astype(np.int16)
     wavfile.write(wav_path, sample_rate, wav)
     logging.info(f"Created wav file at {wav_path}")
-
-
