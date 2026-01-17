6040867a4d6367f51cfb8a8d02f7437b9baac473
yukikongju <emulie.chhor@hotmail.com>
2026-01-16 21:17:51 -0500

    added function to load script

---
+++ CarSegmentation/src/configs/load.py
@@ -0,0 +1,21 @@
+"""
+This file contains utils to load yaml config.
+"""
+
+import os
+import yaml
+
+
+def load_config(config_path: str):
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"The config file {config_path} doesn't exist. Please check!")
+
+    with open(config_path, "r", encoding="utf-8") as f:
+        base = yaml.safe_load(f)
+
+    cfg = {}
+    for entry in base["defaults"]:
+        for key, value in entry.items():
+            cfg[key] = {"name": value, "args": base.get(key, {})}
+
+    return cfg
