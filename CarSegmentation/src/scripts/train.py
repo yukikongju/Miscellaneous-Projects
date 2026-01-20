447c89bd17f1ad7da87cfdb4168fa2086c21a716
yukikongju <emulie.chhor@hotmail.com>
2026-01-20 16:37:01 -0500

    feat: added basic logging for training script

--- CarSegmentation/src/scripts/train.py
+++ CarSegmentation/src/scripts/train.py
@@ -5,6 +5,7 @@ in the yaml file passed as a parameter.
 
 import argparse
 import lightning as L
+import logging
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 
 from configs.load import load_config
@@ -15,11 +16,20 @@ from utils.registry import DATALOADER_REGISTRY, MODEL_REGISTRY, OPTIMIZER_REGIST
 def run():
     parser = argparse.ArgumentParser(description="Script to train model from a yaml config file")
     parser.add_argument("yaml_path", help="Path to yaml. Should be inside 'src/configs'")
+    parser.add_argument("--log_path", help="Path to log output file")
 
     args = parser.parse_args()
-    print(f"{args.yaml_path=}")
+
+    # initializing logger
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename=args.log_path if args.log_path else None,
+    )
 
     # loading config file
+    logging.info(f"Reading model config from {args.yaml_path}")
     cfg = load_config(args.yaml_path)
 
     # load model, dataloader, optimizer, loss function
@@ -45,5 +55,8 @@ def run():
         log_every_n_steps=cfg["trainer"]["log_every_n_steps"],
         callbacks=[EarlyStopping(monitor="val_loss", mode="min")],
     )
+    logging.info("Started model training")
     trainer.fit(model=lightning_module, train_dataloaders=train_loader, val_dataloaders=val_loader)
+
+    logging.info("Started model evaluation")
     trainer.test(model=lightning_module, dataloaders=test_loader)
